6cf1dbd2c3166b3c738f8f740daf65c5d2da91f1
Aaron Schumacher <ajschumacher@gmail.com>
2014-12-12 18:20:44 -0500

    add cider-nrepl

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.2"]]
-  :plugins [[lein-ring "0.8.13"]]
+  :plugins [[lein-ring "0.8.13"]
+            [cider/cider-nrepl "0.8.1"]]
   :ring {:handler mull.core.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
